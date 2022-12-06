9312067a9f8f2c88f6e8df6376b773eb01d5b23e
weaboo699 <maboo681@gmail.com>
2022-12-06 18:50:31 +0800

    zzz

--- app/src/main/java/com/example/myapplication1/zzz.java
+++ app/src/main/java/com/example/myapplication1/zzz.java
@@ -1,5 +1,29 @@
 package com.example.myapplication1;
 
-public class zzz {
-    
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class zzz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("B");
+        setContentView(R.layout.activity_zzz);
+
+        Button btn_to_A = (Button) findViewById(R.id.btn_to_A);
+
+        btn_to_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(zzz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }

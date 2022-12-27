8b30f69f61118d2bcbca860829c55b2b5dd0b51e
weaboo699 <maboo681@gmail.com>
2022-12-28 04:12:44 +0800

    12280412

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,17 +2,77 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.lang.annotation.Target;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner sp;
+    private Spinner sp2;
+    private EditText Level;
+    private EditText Att;
+    private EditText DMG;
+    private EditText FDMG;
+    private EditText BDMG;
+    private EditText CDMG;
+    private EditText Brake;
+    private EditText AD;
+    private EditText Main;
+    private EditText Second;
+    public static String MainClass;
+    public static String MainTarget;
+    public static int MainLevel;
+    public static int MainAtt;
+    public static int MainDMG;
+    public static int MainFDMG;
+    public static int MainBDMG;
+    public static int MainCDMG;
+    public static int MainBrake;
+    public static int MainAD;
+    public static int MainMain;
+    public static int MainSecond;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("A");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Level = (EditText) findViewById(R.id.InLevel);
+        Att = (EditText) findViewById(R.id.InAtt);
+        DMG = (EditText) findViewById(R.id.InDMG);
+        BDMG = (EditText) findViewById(R.id.InBDMG);
+        FDMG = (EditText) findViewById(R.id.InFDMG);
+        CDMG = (EditText) findViewById(R.id.InCDMG);
+        AD = (EditText) findViewById(R.id.InAD);
+        Brake = (EditText) findViewById(R.id.InBrake);
+        Main = (EditText) findViewById(R.id.InMain);
+        Second = (EditText) findViewById(R.id.InSecond);
+        MainLevel =Integer.parseInt(Level.getText().toString());
+        MainAtt =Integer.parseInt(Att.getText().toString());
+        MainAD =Integer.parseInt(AD.getText().toString());
+        MainDMG =Integer.parseInt(DMG.getText().toString());
+        MainBDMG =Integer.parseInt(BDMG.getText().toString());
+        MainCDMG =Integer.parseInt(CDMG.getText().toString());
+        MainFDMG =Integer.parseInt(FDMG.getText().toString());
+        MainBrake =Integer.parseInt(Brake.getText().toString());
+        MainMain =Integer.parseInt(Main.getText().toString());
+        MainSecond =Integer.parseInt(Second.getText().toString());
+
         Button B = (Button) findViewById(R.id.change);
 
         B.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +83,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        sp = (Spinner) findViewById(R.id.InClass);
+
+        // 設定 sp 元件 ItemSelected 事件的 listener
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView parent, View view, int position, long id) {
+                MainClass = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView parent) {
+
+            }
+        });
+        sp2 = (Spinner) findViewById(R.id.InTarget);
+
+        // 設定 sp 元件 ItemSelected 事件的 listener
+        sp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView parent, View view, int position, long id) {
+                MainTarget = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView parent) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/myapplication1/zzz.java
+++ app/src/main/java/com/example/myapplication1/zzz.java
@@ -2,18 +2,75 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class zzz extends AppCompatActivity {
-
+    private String zzzClass;
+    private String zzzTarget;
+    private EditText ADP;
+    private EditText AD;
+    private EditText DMG;
+    private EditText FDMG;
+    private EditText BDMG;
+    private EditText CDMG;
+    private EditText Brake;
+    private EditText Second;
+    private EditText Main;
+    private TextView Answer;
+    private int zzzADP;
+    private int zzzAD;
+    private int zzzDMG;
+    private int zzzFDMG;
+    private int zzzBDMG;
+    private int zzzCDMG;
+    private int zzzBrake;
+    private int zzzMain;
+    private int zzzSecond;
+    private int zzz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("B");
         setContentView(R.layout.activity_zzz);
+
+        Answer = (TextView) findViewById(R.id.AddAnswer);
+        ADP = (EditText) findViewById(R.id.AddADP);
+        AD = (EditText) findViewById(R.id.AddAD);
+        DMG = (EditText) findViewById(R.id.AddDMG);
+        FDMG = (EditText) findViewById(R.id.AddFDMG);
+        BDMG = (EditText) findViewById(R.id.AddBDMG);
+        CDMG = (EditText) findViewById(R.id.AddCDMG);
+        Brake = (EditText) findViewById(R.id.AddBrake);
+        Main = (EditText) findViewById(R.id.AddMain);
+        Second = (EditText) findViewById(R.id.AddSecond);
+        zzzADP =Integer.parseInt(ADP.getText().toString());
+        zzzAD =Integer.parseInt(AD.getText().toString());
+        zzzDMG =Integer.parseInt(DMG.getText().toString());
+        zzzFDMG =Integer.parseInt(FDMG.getText().toString());
+        zzzBDMG =Integer.parseInt(BDMG.getText().toString());
+        zzzCDMG =Integer.parseInt(CDMG.getText().toString());
+        zzzBrake =Integer.parseInt(Brake.getText().toString());
+        zzzMain =Integer.parseInt(Main.getText().toString());
+        zzzSecond =Integer.parseInt(Second.getText().toString());
+
+        Button B = (Button) findViewById(R.id.StartFinal);
+
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Answer.setText(zzz);
+            }
+        });
 
         Button A = (Button) findViewById(R.id.changeB);
 
